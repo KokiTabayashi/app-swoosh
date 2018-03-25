70aeb301bcea12f87742adefbe43148e8675bb82
Koki Tabayashi <koki@K-MacBook-Pro.local>
2018-03-25 13:57:38 -0700

    Data passing through Segue

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Koki Tabayashi on 2018/03/25.
+//  Copyright © 2018年 Koki Tabayashi. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
